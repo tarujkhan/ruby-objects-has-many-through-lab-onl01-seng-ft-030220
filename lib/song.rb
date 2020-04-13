39db2b9e95e1055fc96583e3205b436939af4e42
taroj khan <tarujkhan@yahoo.com>
2020-04-13 16:44:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,2 @@
-class Song 
-  attr_accessor :name, :artist, :genre
-  @@all = []
-  
-  def initialize(name, artist, genre)
-    @name = name
-    @genre = genre
-    @artist = artist
-     
-    @@all << self 
-  end
-  
-  def self.all
-    @@all
-  end 
-end 
-
+ 
   
